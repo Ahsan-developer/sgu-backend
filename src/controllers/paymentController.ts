28ae5f76aae40b7840e879609468c07d8b4d583d
Ahsan-developer <ahsanriaz2466@gmail.com>
2025-04-15 01:11:04 +0500

    ip change

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -87,8 +87,8 @@ export const createAccountLink = async (req: Request, res: Response) => {
   try {
     const accountLink = await stripe.accountLinks.create({
       account: stripeAccountId,
-      return_url: "http://192.168.8.100:3000/stripe-redirect",
-      refresh_url: "http://192.168.8.100:3000/stripe-refresh",
+      return_url: "http://3.27.190.88:3000/stripe-redirect",
+      refresh_url: "http://3.27.190.88:3000:3000/stripe-refresh",
       type: "account_onboarding",
     });
 
--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -29,8 +29,8 @@ export const createCheckoutSession = async (products: any[]) => {
     payment_method_types: ["card"],
     mode: "payment",
     line_items: lineItems,
-    success_url: `http://192.168.8.100:3000/success`,
-    cancel_url: `http://192.168.8.100:3000/cancel`,
+    success_url: `http://3.27.190.88:3000/success`,
+    cancel_url: `http://3.27.190.88:3000/cancel`,
     metadata: {
       vendorProductInfo: JSON.stringify(vendorProductInfo),
     },
